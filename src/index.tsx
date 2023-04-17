b318d6020dff0d76ded9c1f8f5ccde07fcb354d2
asevilla16 <angelsevilla1997@gmail.com>
2023-04-17 16:59:50 -0600

    netlify config

--- src/index.tsx
+++ src/index.tsx
@@ -10,16 +10,13 @@ import "primeicons/primeicons.css";
 import "/node_modules/primeflex/primeflex.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </Provider>
 );
 
